eaa4e38b9c0abed65e6c8e5bfc50e1fefc9749ae
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-11 08:43:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //complete this code
-class Animal {}
+class Animal {
+    constructor(species){
+        this._species = species;
+    }
 
-class Dog extends Animal {}
+    get species(){
+        return `${this._species}`;
+    }
 
-class Cat extends Animal {}
+    makeSound(){
+        console.log(`The ${this._species} makes a sound`);
+    }
+}
+
+class Cat extends Animal {
+    purr(){
+        return 'purr';
+        // console.log('purr');
+        
+    }
+}
+
+class Dog extends Animal {
+    bark(){
+        return 'woof';
+        // console.log('woof');
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
